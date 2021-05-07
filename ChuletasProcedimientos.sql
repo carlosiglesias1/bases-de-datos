9a0985f01d4e70d1643bc937a2f2fd27c2e63149
carlos.iglesiasgomez <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-05-07 19:40:04 +0200

    Joder procedimientos

--- ChuletasProcedimientos.sql
+++ ChuletasProcedimientos.sql
@@ -93,5 +93,88 @@ as
 go
 
 select dbo.librosMes();
-
+go
 exec updateInCaseSoldExemplars;
+go
+--Reducir el precio de los libros de sql en funcion de la editorial
+--si son de Anaya en un 10%
+--de Rama en un 5%
+--los demas no modificarlos
+if exists(select * from sysobjects
+			where name = 'getNameEd')
+	drop function dbo.getNameEd;
+go
+create function getNameEd (@codEd char (3)) returns char (31)
+as
+begin
+	return (select top 1 nombre from Editoriales where Cod_ed = @codEd)
+end
+go
+
+if exists(select * from sysobjects
+			where name = 'updateinCaseEd')
+	drop proc dbo.updateinCaseEd;
+go
+create proc updateinCaseEd 
+as
+	update Libros
+		set Precio *= case
+			when (select dbo.getNameEd(Editorial))='Anaya' then 0.9
+			when (select dbo.getNameEd(Editorial))= 'Rama' then 0.95
+			else 1
+		end
+		where Título like ('%SQL%');
+go
+
+exec updateinCaseEd;
+go
+--Mientras el precio minimo de los libros de una editorial no baje de 10€
+--reducir el precio de sus libros en un 2%, si despues de la 1ª interacción 
+--el precio medio baja de 25€, que pare
+if exists(select * from sysobjects
+			where name = 'dropPriceEd')
+	drop proc dbo.dropPriceEd;
+go
+
+create proc dropPriceEd (@Editorial char (30))
+as
+	declare @codEd char (3)
+	select @codEd = cod_ed from Editoriales where Nombre = 'Anaya';
+	while (select MIN(Precio) from Libros where Editorial = @codEd) > 10
+	begin
+		update Libros
+			set Precio *= 0.98;
+		if((select AVG(Precio) from Libros where Editorial = @codEd)<25)
+			break;
+	end
+go
+
+exec dropPriceEd 'Anaya';
+go
+
+select * from Libros where Editorial = (select cod_ed from Editoriales where Nombre = 'Anaya');
+go
+--Procedimiento que cree la tabla mejores_clientes con el dni, nombre y apellidos de los 10
+--mejores clientes del mes
+if exists(select * from sysobjects
+			where name = 'getBestClients')
+	drop proc dbo.getBestClients;
+go
+
+create proc getBestClients 
+as
+	if exists (select * from sysobjects
+					where name = 'Mejores_Clientes')
+		drop table Mejores_Clientes
+	select top 10 c.dni, c.nombre, c.apellidos into Mejores_Clientes
+		from Clientes c join 
+		Pedidos p on p.Cod_cli = c.Cod_cli
+		join Detalle_pedidos det on p.Num_ped = det.Num_ped
+		join Libros l on l.Cod_lib = det.Cod_lib
+		where MONTH(p.fecha) = MONTH(getdate()) and YEAR(p.fecha) = YEAR(getdate())
+		group by c.DNI, c.Nombre, c.Apellidos
+		order by SUM(l.Precio * det.Num_ej)	
+go
+
+exec getBestClients;
+go
