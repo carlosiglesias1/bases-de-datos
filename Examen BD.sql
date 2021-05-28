e4d4ebc23ac96eae7bd16c7ea2cfac9e2c89fb29
carlosig490@gmail.com <carlosig490@gmail.com>
2021-05-28 18:29:23 +0200

    moito mellor

--- Examen BD.sql
+++ Examen BD.sql
@@ -1,26 +1,25 @@
 use Banco;
 go
 
-/*1.Código de cuenta, fecha de apertura y nombre del cliente, de las cuentas abiertas en el mes en curso por clientes 
-de A Coruña y Lugo cuyo nombre comience por C o D  y tenga en total 5 letras*/
+/*1.Cï¿½digo de cuenta, fecha de apertura y nombre del cliente, de las cuentas abiertas en el mes en curso por clientes 
+de A Coruï¿½a y Lugo cuyo nombre comience por C o D  y tenga en total 5 letras*/
 
 select c.codigo, c.fecha, c.cliente, cli.nombre
 	from Cuentas c join Clientes cli on c.Cliente = cli.Dni
 	where year(c.Fecha) = year(getdate()) and MONTH(c.Fecha) = MONTH(getdate())
-	and cli.Provincia in ('A Coruña', 'Lugo') and len(cli.Nombre)<6 and (cli.Nombre like ('C%') or cli.Nombre like ('D%'));
+	and cli.Provincia in ('A Coruï¿½a', 'Lugo') and len(cli.Nombre)<6 and (cli.Nombre like ('C%') or cli.Nombre like ('D%'));
 go
 
-/*2.Número de cuentas de clientes de A Coruña, con un saldo superior a la media*/
-select AVG(c.saldo)
-	from Cuentas c;
+/*2.Nï¿½mero de cuentas de clientes de A Coruï¿½a, con un saldo superior a la media*/
+declare @avgSaldo int;
+select @avgSaldo = AVG(c.saldo) from Cuentas c;
 go
 select c.codigo
 	from Cuentas c join Clientes cli on c.Cliente = cli.Dni
-	where cli.Provincia = 'A Coruña' and c.Saldo > (select AVG(c.saldo)
-														from Cuentas c);
+	where cli.Provincia = 'A Coruï¿½a' and c.Saldo > @avgSaldo;
 go
 
-/*3.Dni de los  clientes de A Coruña que tienen mas de 1 cuenta con un saldo superior a la media?*/
+/*3.Dni de los  clientes de A Coruï¿½a que tienen mas de 1 cuenta con un saldo superior a la media?*/
 select cli.dni
 	from Clientes cli join Cuentas c on cli.Dni = c.Cliente;
 go
@@ -33,17 +32,17 @@ select top 3 SUM(c.saldo) as 'Saldo total', cli.nombre, cli.Dni into Top_3_Clien
 	order by SUM(c.saldo);
 go
 
-/*5.Borrar  todas las cuentas que no han tenido ningún movimiento en los últimos 1000 dias, incluyendo las que nunca han tenido ningún movimiento.*/
+/*5.Borrar  todas las cuentas que no han tenido ningï¿½n movimiento en los ï¿½ltimos 1000 dias, incluyendo las que nunca han tenido ningï¿½n movimiento.*/
 
-/*6.¿Cual fué el ingreso máximo, el mínimo y el medio del día de hoy?*/
-select top 1 m.importe as 'máximo', AVG(m.importe) as 'medio' 
+/*6.ï¿½Cual fuï¿½ el ingreso mï¿½ximo, el mï¿½nimo y el medio del dï¿½a de hoy?*/
+select top 1 m.importe as 'mï¿½ximo', AVG(m.importe) as 'medio' 
 	from Movimientos m
 	where m.Tipo = 'i' and m.Fecha = GETDATE()
 	group by m.Importe;
 go
 
-/*7.Dni y nombre del cliente, número de cuentas que tiene, saldo total de todas sus cuentas, de los que tienen un saldo total superior a 
-2000€ y cuyo nombre comience por a, b o c*/
+/*7.Dni y nombre del cliente, nï¿½mero de cuentas que tiene, saldo total de todas sus cuentas, de los que tienen un saldo total superior a 
+2000ï¿½ y cuyo nombre comience por a, b o c*/
 select cli.dni, cli.nombre, count(c.codigo) as Cuentas, sum(c.saldo) as Saldo_Total
 	from Clientes cli join Cuentas c on c.Cliente = cli.Dni
 	where cli.Nombre like ('A%') or cli.Nombre like ('B%') or cli.Nombre like ('C%')
@@ -52,24 +51,24 @@ select cli.dni, cli.nombre, count(c.codigo) as Cuentas, sum(c.saldo) as Saldo_To
 go
 
 /*
-8.A todas las cuentas abiertas este mes, de clientes de A Coruña y Lugo, que tengan un saldo por encima de la media, aumentarles el saldo en 100€
+8.A todas las cuentas abiertas este mes, de clientes de A Coruï¿½a y Lugo, que tengan un saldo por encima de la media, aumentarles el saldo en 100ï¿½
 */
 update Cuentas
 	set Saldo = Saldo + 100
 	from Cuentas join Clientes on Cuentas.Cliente = Clientes.Dni 
 	where Saldo > (select avg(saldo)
 					from Cuentas)
-		and Provincia in ('A Coruña', 'Lugo');
+		and Provincia in ('A Coruï¿½a', 'Lugo');
 go
 --Comprobamos--
 select * from Cuentas c join Clientes cli on c.Cliente = cli.Dni
-	where cli.Provincia in ('A Coruña', 'Lugo')
+	where cli.Provincia in ('A Coruï¿½a', 'Lugo')
 		and c.Saldo > (select avg(saldo)
 							from Cuentas);
 go
 
 /*
-9.Eliminar todos los ingresos del día de hoy, en la cuenta de código 1, de importe inferior a 10€
+9.Eliminar todos los ingresos del dï¿½a de hoy, en la cuenta de cï¿½digo 1, de importe inferior a 10ï¿½
 */
 delete from Movimientos
  where codigo = 1 and Importe < 10;
@@ -78,7 +77,7 @@ delete from Movimientos
  go
 
  /*
- 10.A todas las cuentas que han tenido mas de 2 ingresos en el día de hoy, por un importe total superior a 1000€, aumentarles 100€ en el saldo.
+ 10.A todas las cuentas que han tenido mas de 2 ingresos en el dï¿½a de hoy, por un importe total superior a 1000ï¿½, aumentarles 100ï¿½ en el saldo.
  */
  select * from Cuentas;
  update Cuentas
